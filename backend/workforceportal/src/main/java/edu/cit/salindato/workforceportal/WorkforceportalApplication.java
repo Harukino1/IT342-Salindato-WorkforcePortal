6e866742179b450d54ce7e60f1100c0421c1f942
LAB13_USER ON WS09 <L13Y12W09@LabHCI.net>
2026-03-07 11:58:22 +0800

    Initial commit: backend and frontend

---
+++ backend/workforceportal/src/main/java/edu/cit/salindato/workforceportal/WorkforceportalApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.salindato.workforceportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceportalApplication.class, args);
+	}
+
+}
---
+++ backend/workforceportal/src/test/java/edu/cit/salindato/workforceportal/WorkforceportalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.salindato.workforceportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

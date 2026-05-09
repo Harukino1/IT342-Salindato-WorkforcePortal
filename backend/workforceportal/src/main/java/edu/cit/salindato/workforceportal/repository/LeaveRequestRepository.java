1b0ea7b3c3694c2b0a881bf63daeb8c127a6bad1
Harukino1 <128798709+Harukino1@users.noreply.github.com>
2026-05-09 11:00:00 +0800

    refactor: removed duplicates

--- backend/workforceportal/src/main/java/edu/cit/salindato/workforceportal/repository/LeaveRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.cit.salindato.workforceportal.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import edu.cit.salindato.workforceportal.model.LeaveRequest;
-
-public interface LeaveRequestRepository extends MongoRepository<LeaveRequest, String> {
-}
